5c904067fe0d8f1f99e436b3b8d16a1840cece72
Gami <gamiofficial0@gmail.com>
2023-02-10 04:31:48 +0100

    attempt at fixing guildlist overflow

--- src/Components/Guild/Guild.tsx
+++ src/Components/Guild/Guild.tsx
@@ -1,5 +1,6 @@
 // SolidJS
 import { A } from '@solidjs/router';
+import { createEffect, onMount } from 'solid-js';
 
 // API
 import { useAppState } from '../../AppState';
@@ -13,18 +14,37 @@ interface GuildProps {
 }
 
 const Guild = (props: GuildProps) => {
+	let pos: HTMLElement | any;
+	let span: HTMLElement | any;
+	let before: HTMLElement | any;
 	const AppState: any = useAppState();
+
 	const guild = AppState.userGuilds().find((x: GuildType) => x.id === props.id);
+	onMount(() => {
+		const observer = new ResizeObserver(([e]) => {
+			/* set span position to pos position */
+
+			span.style.top = `${
+				pos.getBoundingClientRect().y + span.getBoundingClientRect().height / 6
+			}px`;
+			before.style.top = `${
+				pos.getBoundingClientRect().y +
+				before.getBoundingClientRect().height / 6
+			}px`;
+		});
+		observer.observe(pos);
+	});
 
 	return (
-		<li class={style.guild}>
+		<li ref={pos} class={style.guild}>
+			<div ref={before} class={style.before}></div>
 			<A href={`${props.id}/${guild.system_channel_id}`}>
 				<img
 					src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.webp?size=96`}
 					alt={guild.name}
 				/>
 			</A>
-			<span>{guild.name}</span>
+			<span ref={span}>{guild.name}</span>
 		</li>
 	);
 };
