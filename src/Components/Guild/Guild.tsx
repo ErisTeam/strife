b0d2db27b9b45d369363d98847ca973b483ac0f8
Frytak <frytak2855@gmail.com>
2023-03-03 20:58:57 +0100

    GuildList CSS

--- src/Components/Guild/Guild.tsx
+++ src/Components/Guild/Guild.tsx
@@ -13,30 +13,16 @@ interface GuildProps {
 }
 
 const Guild = (props: GuildProps) => {
-	let pos: HTMLElement | any;
-	let span: HTMLElement | any;
-	let before: HTMLElement | any;
 	const AppState: any = useAppState();
 
 	const guild = AppState.userGuilds().find((x: GuildType) => x.id === props.id);
-	// onMount(() => {
-	// 	const observer = new ResizeObserver(([e]) => {
-	// 		/* set span position to pos position */
-
-	// 		span.style.top = `${pos.getBoundingClientRect().y + span.getBoundingClientRect().height / 6}px`;
-	// 		before.style.top = `${pos.getBoundingClientRect().y + before.getBoundingClientRect().height / 6}px`;
-	// 	});
-	// 	observer.observe(pos);
-	// });
 
 	return (
-		<>
-			{/* <div ref={before} class={style.before}></div> */}
+		<li>
 			<a class={style.guild} href={`${props.id}/${guild.system_channel_id}`}>
 				<img src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.webp?size=96`} alt={guild.name} />
 			</a>
-			{/* <span ref={span}>{guild.name}</span> */}
-		</>
+		</li>
 	);
 };
 
