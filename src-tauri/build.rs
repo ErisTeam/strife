68a1b3abd06c488cb96009d75d6e6dc1a4a40cbb
pietruszka123 <56603721+pietruszka123@users.noreply.github.com>
2023-06-01 16:24:17 +0200

    protobuf

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,7 +1,21 @@
 use std::io::Result;
 fn main() -> Result<()> {
-    prost_build::compile_protos(&["src/proto/PreloadedUserSettings.proto"], &["src/"])?;
+	proto_buf()?;
+	tauri_build::build();
+	Ok(())
+}
 
-    tauri_build::build();
-    Ok(())
+fn proto_buf() -> Result<()> {
+	if cfg!(target_os = "windows") {
+		//check if protoc exists
+		std::env::set_var("PROTOC", "../protoc-23.2/win64/protoc");
+	} else {
+		println!("Using protoc from PATH");
+	}
+
+	let mut prost_config = prost_build::Config::new();
+	prost_config.protoc_arg("--proto_path=../protoc-23.2/include/");
+
+	prost_config.compile_protos(&["src/proto/PreloadedUserSettings.proto"], &["src/"])?;
+	Ok(())
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,4 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
 
 mod tests;
 
@@ -24,64 +21,58 @@ extern crate tokio;
 
 use std::sync::Arc;
 
-use log::{debug, error, info, warn};
+use log::{ debug, error, info, warn };
 use serde::Deserialize;
-use tauri::{Manager, State, UserAttentionType};
+use tauri::{ Manager, State, UserAttentionType };
 use tauri_plugin_log::LogTarget;
 
-use crate::{main_app_state::MainState, manager::ThreadManager};
+use crate::{ main_app_state::MainState, manager::ThreadManager };
 
 #[tauri::command]
 fn set_state(new_state: String, state: State<Arc<MainState>>, handle: tauri::AppHandle) {
-    debug!("change state {}", new_state);
-    match new_state.as_str() {
-        "Application" => {
-            println!("Application");
-            if matches!(
-                *state.state.lock().unwrap(),
-                main_app_state::State::MainApp { .. }
-            ) {
-                warn!("Already in main app");
-                return;
-            }
-            state.change_state(main_app_state::State::MainApp {}, handle, false)
-        }
-        "LoginScreen" => {
-            if matches!(
-                *state.state.lock().unwrap(),
-                main_app_state::State::LoginScreen { .. }
-            ) {
-                warn!("Already in login screen");
-                return;
-            }
-            state.change_state(main_app_state::State::default_login_screen(), handle, false);
-        }
-        _ => {
-            error!("Unknown state {}", new_state);
-        }
-    }
+	debug!("change state {}", new_state);
+	match new_state.as_str() {
+		"Application" => {
+			println!("Application");
+			if matches!(*state.state.lock().unwrap(), main_app_state::State::MainApp { .. }) {
+				warn!("Already in main app");
+				return;
+			}
+			state.change_state(main_app_state::State::MainApp {}, handle, false)
+		}
+		"LoginScreen" => {
+			if matches!(*state.state.lock().unwrap(), main_app_state::State::LoginScreen { .. }) {
+				warn!("Already in login screen");
+				return;
+			}
+			state.change_state(main_app_state::State::default_login_screen(), handle, false);
+		}
+		_ => {
+			error!("Unknown state {}", new_state);
+		}
+	}
 }
 
 #[tauri::command]
 fn get_token(user_id: String, state: State<Arc<MainState>>) -> Option<String> {
-    state.get_token(user_id)
+	state.get_token(user_id)
 }
 
 #[tauri::command]
 fn get_last_user(state: State<Arc<MainState>>) -> Option<String> {
-    println!("get_last_user");
-    println!("{:?}", state.last_id.lock().unwrap());
-    state.last_id.lock().unwrap().clone()
+	println!("get_last_user");
+	println!("{:?}", state.last_id.lock().unwrap());
+	state.last_id.lock().unwrap().clone()
 }
 #[tauri::command]
 async fn close_splashscreen(window: tauri::Window) {
-    // Close splashscreen
-    if let Some(splashscreen) = window.get_window("splashscreen") {
-        splashscreen.close().unwrap();
-    }
-    // Show main window
-    window.get_window("main").unwrap().show().unwrap();
-    println!("Closing Loading Screen");
+	// Close splashscreen
+	if let Some(splashscreen) = window.get_window("splashscreen") {
+		splashscreen.close().unwrap();
+	}
+	// Show main window
+	window.get_window("main").unwrap().show().unwrap();
+	println!("Closing Loading Screen");
 }
 
 #[derive(Debug, Deserialize)]
@@ -90,75 +81,65 @@ struct TestData {}
 #[cfg(debug_assertions)]
 #[tauri::command]
 async fn test(handle: tauri::AppHandle) {
-    println!("test");
+	println!("test");
 
-    // let mut path = handle.path_resolver().app_cache_dir().unwrap();
-    // path.push("413428675866787863");
-    // path.set_extension("webp");
-    // println!("{:?}", path);
-    let windows = handle.windows();
-    let window = windows.iter().next().unwrap().1;
-    tokio::time::sleep(Duration::from_millis(1000)).await;
-    window
-        .request_user_attention(Some(UserAttentionType::Informational))
-        .unwrap();
-    println!("test");
+	// let mut path = handle.path_resolver().app_cache_dir().unwrap();
+	// path.push("413428675866787863");
+	// path.set_extension("webp");
+	// println!("{:?}", path);
+	let windows = handle.windows();
+	let window = windows.iter().next().unwrap().1;
+	tokio::time::sleep(Duration::from_millis(1000)).await;
+	window.request_user_attention(Some(UserAttentionType::Informational)).unwrap();
+	println!("test");
 
-    //use notify_rust::Notification;
+	//use notify_rust::Notification;
 
-    use std::time::Duration;
+	use std::time::Duration;
 
-    use winrt_notification::Toast;
+	use winrt_notification::Toast;
 
-    //notifications::new_message(Message::default(), &handle, None).await;
+	//notifications::new_message(Message::default(), &handle, None).await;
 }
 
 fn main() {
-    println!("Starting");
+	println!("Starting");
 
-    let main_state = Arc::new(MainState::new());
+	let main_state = Arc::new(MainState::new());
 
-    let thread_manager = ThreadManager::new(main_state.clone());
+	let thread_manager = ThreadManager::new(main_state.clone());
 
-    let event_manager = event_manager::EventManager::new(main_state.clone());
+	let event_manager = event_manager::EventManager::new(main_state.clone());
 
-    *main_state.thread_manager.lock().unwrap() = Some(thread_manager);
+	*main_state.thread_manager.lock().unwrap() = Some(thread_manager);
 
-    *main_state.event_manager.lock().unwrap() = Some(event_manager);
+	*main_state.event_manager.lock().unwrap() = Some(event_manager);
 
-    let m = main_state.clone();
+	let m = main_state.clone();
 
-    #[cfg(debug_assertions)]
-    tauri::Builder::default()
-        .plugin(
-            tauri_plugin_log::Builder::default()
-                .targets([LogTarget::LogDir, LogTarget::Stdout, LogTarget::Webview])
-                .build(),
-        )
-        .manage(main_state)
-        .setup(move |app| {
-            let app_handle = app.handle();
-            test::add_token(&m, app_handle.clone());
-            m.change_state(
-                main_app_state::State::default_login_screen(),
-                app_handle,
-                false,
-            );
+	#[cfg(debug_assertions)]
+	tauri::Builder
+		::default()
+		.plugin(
+			tauri_plugin_log::Builder
+				::default()
+				.targets([LogTarget::LogDir, LogTarget::Stdout, LogTarget::Webview])
+				.build()
+		)
+		.manage(main_state)
+		.setup(move |app| {
+			let app_handle = app.handle();
+			test::add_token(&m, app_handle.clone());
+			m.change_state(main_app_state::State::default_login_screen(), app_handle, false);
 
-            let splashscreen_window = app.get_window("splashscreen").unwrap();
-            let main_window = app.get_window("main").unwrap();
-            splashscreen_window.close().unwrap();
-            main_window.show().unwrap();
+			let splashscreen_window = app.get_window("splashscreen").unwrap();
+			let main_window = app.get_window("main").unwrap();
+			splashscreen_window.close().unwrap();
+			main_window.show().unwrap();
 
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![
-            get_token,
-            set_state,
-            get_last_user,
-            close_splashscreen,
-            test
-        ])
-        .run(tauri::generate_context!())
-        .expect("Error while running tauri application.");
+			Ok(())
+		})
+		.invoke_handler(tauri::generate_handler![get_token, set_state, get_last_user, close_splashscreen, test])
+		.run(tauri::generate_context!())
+		.expect("Error while running tauri application.");
 }
