cfe11048f169715ddd364589faa415f378d079e3
pietruszka123 <56603721+pietruszka123@users.noreply.github.com>
2023-07-09 14:31:24 +0200

    .

--- src/Translation.tsx
+++ src/Translation.tsx
@@ -1,12 +1,18 @@
-import en_us from './Locales/en_US.json';
+import en_us from './Locales/en_US';
 
 import { createChainedI18nContext, useI18n } from '@solid-primitives/i18n';
+import pl_PL from './Locales/pl_PL';
+
+export const dictionaries = {
+	en_US: en_us,
+
+	pl_PL: pl_PL,
+};
 
 const [I18nProvider, useI18nContext] = createChainedI18nContext(
 	{
-		dictionaries: {
-			en_US: en_us,
-		},
+		//@ts-ignore
+		dictionaries: dictionaries,
 		locale: 'en_US', // Starting locale
 	},
 	true
@@ -16,17 +22,5 @@ export const useTrans = () => {
 	const context = useI18nContext();
 	if (!context) throw new Error('useI18n must be used within an I18nProvider');
 	return context;
-};
-
-export const addLocale = async (locale: string) => {
-	if (typeof locale !== 'string') throw new Error('Locale must be a string');
-
-	const [_, { add, dict }] = useI18n();
-
-	let localeData = await import(`./Locales/${locale}.json`);
-	if (dict(locale)) {
-		console.warn(`Locale ${locale} already exists!`);
-	}
-	add(locale, localeData);
 };
 export const TransProvider = I18nProvider;
