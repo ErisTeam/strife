96d8325c7e018471dd46822323e30ce848c3e6b4
Gami <gamiofficial0@gmail.com>
2023-08-22 19:53:41 +0200

    gami is big dumb dumb and forgot spoiler

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,12 @@ import type { StorybookConfig } from 'storybook-solidjs-vite';
 
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-a11y',
+	],
 	framework: {
 		name: 'storybook-solidjs-vite',
 		options: {},
