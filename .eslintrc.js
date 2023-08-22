122c285b93e55080fe2821389a2cbe758e95caf8
Gami <gamiofficial0@gmail.com>
2023-08-22 19:24:57 +0200

    storybook set up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
 	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking',
-	],
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:storybook/recommended"
+    ],
 	parser: '@typescript-eslint/parser',
 
 	parserOptions: {
---
+++ .storybook/preview.ts
@@ -0,0 +1,11 @@
+import { themes } from '@storybook/theming';
+
+const preview = {
+	parameters: {
+		docs: {
+			theme: themes.dark,
+		},
+	},
+};
+
+export default preview;
