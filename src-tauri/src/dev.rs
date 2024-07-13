328a844861dfd60d1c3255489f0e4716ae9162cb
pietruszka123 <56603721+pietruszka123@users.noreply.github.com>
2024-07-13 18:57:40 +0200

    .

--- src-tauri/src/dev.rs
+++ src-tauri/src/dev.rs
@@ -47,42 +47,42 @@ pub fn clear_gateway_logs(handle: AppHandle) {
 	let _ = fs::create_dir(path);
 }
 
-pub async fn gami_to_furras(token: String, gami: bool) {
-	use base64::Engine;
-	use prost::Message;
-	use serde_json::json;
-	use crate::discord::types::protos::discord_protos::discord_users::v1::preloaded_user_settings::{
-		PreloadedUserSettings,
-		preloaded_user_settings::{ StatusSettings, CustomStatus },
-	};
-	let mut settings = PreloadedUserSettings::default();
-	let mut status_settings = StatusSettings {
-		status: Some("online".to_string()),
-		..Default::default()
-	};
-	let mut status = CustomStatus::default();
-	if gami {
-		status.text = "yo".to_string();
-		status.emoji_name = "💀".to_string();
-	} else {
-		status.text = "yoyo".to_string();
-	}
+// pub async fn gami_to_furras(token: String, gami: bool) {
+// 	use base64::Engine;
+// 	use prost::Message;
+// 	use serde_json::json;
+// 	use crate::discord::types::protos::discord_protos::discord_users::v1::preloaded_user_settings::{
+// 		PreloadedUserSettings,
+// 		preloaded_user_settings::{ StatusSettings, CustomStatus },
+// 	};
+// 	let mut settings = PreloadedUserSettings::default();
+// 	let mut status_settings = StatusSettings {
+// 		status: Some("online".to_string()),
+// 		..Default::default()
+// 	};
+// 	let mut status = CustomStatus::default();
+// 	if gami {
+// 		status.text = "yo".to_string();
+// 		status.emoji_name = "💀".to_string();
+// 	} else {
+// 		status.text = "yoyo".to_string();
+// 	}
 
-	status_settings.custom_status = Some(status);
+// 	status_settings.custom_status = Some(status);
 
-	settings.status = Some(status_settings);
+// 	settings.status = Some(status_settings);
 
-	let mut buf = Vec::new();
-	settings.encode(&mut buf).unwrap();
+// 	let mut buf = Vec::new();
+// 	settings.encode(&mut buf).unwrap();
 
-	let encoded = base64::engine::general_purpose::STANDARD.encode(buf);
+// 	let encoded = base64::engine::general_purpose::STANDARD.encode(buf);
 
-	let client = reqwest::Client::new();
-	let res = client
-		.patch("https://discord.com/api/v9/users/@me/settings-proto/1")
-		.header("Authorization", token)
-		.json(&json!({ "settings": encoded }))
-		.send().await
-		.unwrap();
-	println!("{:?}", res.text().await);
-}
+// 	let client = reqwest::Client::new();
+// 	let res = client
+// 		.patch("https://discord.com/api/v9/users/@me/settings-proto/1")
+// 		.header("Authorization", token)
+// 		.json(&json!({ "settings": encoded }))
+// 		.send().await
+// 		.unwrap();
+// 	println!("{:?}", res.text().await);
+// }
--- src-tauri/src/discord/types/protos.rs
+++ src-tauri/src/discord/types/protos.rs
@@ -1,12 +1,12 @@
-pub mod discord_protos {
-	pub mod discord_users {
-		pub mod v1 {
-			pub mod preloaded_user_settings {
-				include!(concat!(env!("OUT_DIR"), "/discord_protos.discord_users.v1.preloaded_user_settings.rs"));
-			}
-		}
-	}
-}
+// pub mod discord_protos {
+// 	pub mod discord_users {
+// 		pub mod v1 {
+// 			pub mod preloaded_user_settings {
+// 				include!(concat!(env!("OUT_DIR"), "/discord_protos.discord_users.v1.preloaded_user_settings.rs"));
+// 			}
+// 		}
+// 	}
+// }
 
 pub mod d {
 	include!(concat!(env!("OUT_DIR"), "/d.rs"));
