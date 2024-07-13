af4c9e7158a578f59513cc17034b0784234aa966
Gami <gamiofficial0@gmail.com>
2024-07-13 21:02:05 +0200

    upgraded versions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -68,7 +68,8 @@ fn close_loading(app: &mut tauri::App) -> Result<()> {
 fn enable_round_borders(window: tauri::Window) {
 	use log::debug;
 	use windows::Win32::{ Graphics::Dwm::DwmExtendFrameIntoClientArea, UI::Controls::MARGINS };
-	let hwnd = windows::Win32::Foundation::HWND(window.hwnd().unwrap().0);
+	
+	let hwnd = windows::Win32::Foundation::HWND(window.hwnd().unwrap().0 as _) ;
 	let margins = MARGINS {
 		cxLeftWidth: 1,
 		cxRightWidth: 1,
