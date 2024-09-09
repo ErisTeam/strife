3698d736df8c5a40d02e6285e0009e5e0a862ac6
Gami <gamiofficial0@gmail.com>
2024-09-09 18:26:54 +0200

    Add initial project files for native

---
+++ strife/windows/strife/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::strife::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::strife::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
